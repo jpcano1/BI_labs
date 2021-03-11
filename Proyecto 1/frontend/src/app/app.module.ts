942e650aeb969d1331cea19935a8c049dd5d1f2a
Juan Pablo <jp.cano@uniandes.edu.co>
2021-03-11 01:09:18 -0500

    Animations Enabled

--- Proyecto 1/frontend/src/app/app.module.ts
+++ Proyecto 1/frontend/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LabelerComponent } from './labeler/labeler.component';
 import { PredictionComponent } from './prediction/prediction.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { NavigatorComponent } from './navigator/navigator.component';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     LabelerComponent,
-    PredictionComponent
+    PredictionComponent,
+    NavigatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
